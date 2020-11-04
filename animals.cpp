51b78da29e1418ae72dc3171eb5c8aeac6f01792
Nastya Tretyakova <42992831+fabulousananas@users.noreply.github.com>
2020-11-04 14:21:51 +0300

    Update animals.cpp

--- animals.cpp
+++ animals.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include "header.h"
 
+using namespace std;
+
+int chosen_number;
+
+void menu()
+{
+    cout << "   МЕНЮ\n"
+        << "(0) Выход из программы. \n"
+        << "(1) Добавление новых элементов. \n"
+        << "(2) Удаление элементов. \n"
+        << "(3) Редактирование элементов. \n"
+        << "(4) Сортировка элементов. \n"
+        << "(5) Вывод информации. \n"
+        << "Выбор: \n";
+    cin >> chosen_number;
+}
+
+void main()
+{
+    setlocale(LC_ALL, "RUS");
+
+
+}
