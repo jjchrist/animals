7cfb2307ec2acc1a5d3a11522e169f0fb08daad5
Nastya Tretyakova <42992831+fabulousananas@users.noreply.github.com>
2020-11-04 17:16:27 +0300

    Update header.h

--- header.h
+++ header.h
@@ -1,1 +1,39 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
+class List
+{
+	class Animal
+	{
+		int age;
+		int weight;
+		string animal;
+		Animal* next;
+	public:
+		void setName(string animal);
+		string getName();
+		void setAge(int age);
+		int getAge();
+		void setWeight(int weight);
+		int getWeight();
+		void setNext(Animal* next);
+		Animal* getNext();
+	};
+	
+protected:
+	Animal* head;
+	void setHead(Animal* head);
+	Animal* getHead();
+public:
+	List() 
+	{
+		head = NULL;
+	}
+	bool Empty();
+	void Add(string animal, int age, int weight);
+	//void Delete(string animal);
+	void Print();
+	//void Sort(int weight);
+};
